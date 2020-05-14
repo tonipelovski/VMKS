1af7d48045c510b241b64aa987b667f0c2fc09c7
Martin Stamenkov <bul2013@abv.bg>
2020-05-14 15:05:34 +0300

    added final version

--- embedded/vmks.ino
+++ embedded/vmks.ino
@@ -3,6 +3,9 @@ int bluetoothTx = 2; // D2 to TXD
 
 int bluetoothRx = 3; // D3 to RXD  (Warning! See Text)
 
+int status = 0;
+int current_status = 0;
+
 SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 
 void setup()
@@ -14,27 +17,51 @@ void setup()
   delay(100);
   bluetooth.println("U,9600,N");
   bluetooth.begin(9600);
-
+  pinMode(12, INPUT);
+  if(digitalRead(12)){
+      bluetooth.println("       locked");
+      status = 1;
+  }else{
+      bluetooth.println("       unlocked");
+      status = 0;
+  }
 }
 
 void loop()
 
 {
+  
   if (bluetooth.available())
 
   {
     char response  =(char) bluetooth.read();
     Serial.print(response);
-    if(response == '1')digitalWrite(13,HIGH);
-    else if(response == '0')digitalWrite(13,LOW);
+    if(response == '1'){
+      digitalWrite(13,HIGH);
+      current_status = 1;
+    }
+    else if(response == '0'){
+      digitalWrite(13,LOW);
+      current_status = 0;
+    }
 
   }
-
+  if(status != current_status){
+    if(digitalRead(12)){
+        bluetooth.println("        locked");
+        Serial.println("locked");
+    }else{
+        bluetooth.println("        unlocked");
+        Serial.println("unlocked");
+    }
+  }
+  
   if (Serial.available())
 
   {
-
     bluetooth.print((char)Serial.read());
-
   }
+  status = current_status;
+
+
 }
